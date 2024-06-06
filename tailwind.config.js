3a4720b2a0f1de43f57231ad1224fc72e9686c32
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-06-06 18:45:58 +0200

    style: fonts & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'bodoni': 'Bodoni_Moda'
+    },
+    extend: {
+      colors: {
+        'white': '#F5F5F5',
+        'light': '#EEE6DB',
+        'primary': '#BBA381', 
+        'secondary': '#846346', 
+        'dark': '#5B5B5B', 
+        'black': '#0F0E0E'
+      }
+    },
   },
   plugins: [],
 }
