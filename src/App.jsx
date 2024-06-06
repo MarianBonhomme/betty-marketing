810d0fa64480a19196086686b2acbb22b6030640
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-06-06 18:47:19 +0200

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import About from "./components/About";
 import Benefits from "./components/Benefits";
 import Headline from "./components/Headline";
-import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div className="max-w-screen font-bodoni text-dark">
-      <Sidebar />
+      <Header />
       <Headline />
       <Benefits />
       <About />
---
+++ src/components/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Text from "./Text";
+import Button from "./Button";
+
+export default function About() {
+  return (
+    <div className="flex items-center justify-center gap-20 bg-black p-20 shadow-2xl">
+      <div className="flex h-[375px] w-[375px] items-center justify-center relative">
+        <img src="src/assets/images/ornements-2.png" alt="ornements" className="absolute top-0 left-0 w-full h-full " />
+        <div className="h-[250px] w-[200px] -rotate-3 bg-white pt-[15px]">
+          <img
+            src="src/assets/images/btissam.jpg"
+            alt="Btissam"
+            className="mx-auto h-[170px] w-[170px]"
+          />
+          <p className="mt-3 text-center text-xl/5 font-bold text-secondary">
+            Btissam Aboubeker Lamhamdi
+          </p>
+        </div>
+      </div>
+      <div className="flex w-1/2 flex-col gap-5">
+        <p className="text-4xl font-bold italic text-primary">Qui suis-je?</p>
+        <div className="grid gap-3 text-justify text-white">
+          <Text>
+            Après avoir lancé leur entreprise, de nombreuses personnes
+            abandonnent rapidement la gestion de leurs réseaux sociaux par
+            manque de temps ou de compétences. Cela semble simple mais en
+            réalité, c’est une lourde responsabilité qui requiert une quantité
+            significative de temps et de savoir-faire.
+          </Text>
+          <Text>
+            Betty Marketing digital est un service de Community Management (CM)
+            chargé de gérer et animer vos réseaux sociaux d’entreprise ou de
+            marque. Par conséquent, j’assure la préservation de votre
+            e-réputation en ligne. A cet effet, Betty Marketing Digital, vous
+            propose un service de CM, quel que soit votre domaine d’activité.
+          </Text>
+        </div>
+        <div className="mt-2 flex items-end justify-between">
+          <Button
+            text={"Contactez-moi"}
+            css={"bg-primary text-black font-bold text-sm"}
+          />
+          <ul className="flex items-center justify-end gap-5">
+            <img
+              src="src/assets/images/instagram-primary.svg"
+              alt="instagram"
+              className="h-6 w-6 cursor-pointer"
+            />
+            <img
+              src="src/assets/images/threads-primary.svg"
+              alt="threads"
+              className="h-6 w-6 cursor-pointer"
+            />
+            <img
+              src="src/assets/images/facebook-primary.svg"
+              alt="facebook"
+              className="h-6 w-6 cursor-pointer"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
