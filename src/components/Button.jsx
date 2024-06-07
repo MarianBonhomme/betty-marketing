250cbd72220497b34b2821f0aa74638288f915bb
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-06-07 14:29:10 +0200

    style components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ text, clicked, css }) {
   return (
     <div
-      className={`max-w-fit cursor-pointer bg-secondary px-5 py-3 uppercase text-white tracking-widest ${css && css} transition hover:brightness-125`}
+      className={`max-w-fit cursor-pointer px-5 py-3 uppercase text-white tracking-widest ${css && css} transition hover:brightness-125`}
       onClick={clicked}
     >
       {text}
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Text({ children, css }) {
-  return <p className={`text-lg/6 ${css && css}`}>{children}</p>;
+  return <p className={`text-base/5 ${css && css}`}>{children}</p>;
 }
