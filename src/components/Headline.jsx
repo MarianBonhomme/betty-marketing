7e9ac86cab93995608d74b73c4451b8109fe6746
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-06-07 14:29:05 +0200

    style headline

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -7,14 +7,14 @@ export default function Headline() {
   return (
     <div className="grid grid-cols-2 items-center gap-10 p-20">
       <div className="flex flex-col gap-5">
-        <Title>Développe tes réseaux-sociaux et améliore ta notoriété</Title>
+        <Title>développons ensemble votre succès en ligne</Title>
         <Text>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis
           ad architecto, voluptas quidem ex fugit commodi reiciendis quod
           deleniti possimus deserunt veritatis earum natus reprehenderit aliquam
           totam, fugiat est voluptates?
         </Text>
-        <Button text={"en savoir plus"} css={"mt-2"} />
+        <Button text={"en savoir plus"} css={"bg-secondary mt-2"} />
       </div>
       <div className="h-[400px] w-full border border-dark"></div>
     </div>
